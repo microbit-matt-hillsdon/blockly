b8c7dc263c41b17abc962724ae160ea88c6771a6
Robert Knight <robert@microbit.org>
2025-12-09 16:21:46 +0000

    Use button role for dropdown element
    The semantics are still good, and this avoids odd output
    on VoiceOver when accepting a value "collapsed, completion
    selected"

--- core/field_dropdown.ts
+++ core/field_dropdown.ts
@@ -209,7 +209,7 @@ export class FieldDropdown extends Field<string> {
   protected recomputeAria() {
     if (!this.fieldGroup_) return; // There's no element to set currently.
     const element = this.getFocusableElement();
-    aria.setRole(element, aria.Role.COMBOBOX);
+    aria.setRole(element, aria.Role.BUTTON);
     aria.setState(element, aria.State.HASPOPUP, aria.Role.LISTBOX);
     aria.setState(element, aria.State.EXPANDED, !!this.menu_);
     if (this.menu_) {
