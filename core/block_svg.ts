3c218a7896f42450d81944eef257906b714aef12
Robert Knight <robert@microbit.org>
2025-12-18 14:35:47 +0000

    Fix full block field labels in flyout

--- core/block_svg.ts
+++ core/block_svg.ts
@@ -234,7 +234,7 @@ export class BlockSvg
    * @internal
    */
   recomputeAriaLabel() {
-    if (this.isSimpleReporter(true, true)) return;
+    if (this.isSimpleReporter(true, true) && !this.isInFlyout) return;
 
     if (
       this.isSimpleReporterShadowBlockWithFullBlockField() &&
